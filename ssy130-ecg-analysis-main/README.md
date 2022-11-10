05e211a24852b8697f5195d242f30c2efa218617
rockofut17 <rodrigo_sanco@outlook.com>
2022-11-10 13:24:58 +0100

    Add files via upload

---
+++ ssy130-ecg-analysis-main/README.md
@@ -0,0 +1,2 @@
+# SSY130 ECG Analysis
+Files for project 1 in SSY130 Applied Signal Processing
---
+++ ssy130-ecg-analysis-main/matlab/sincos.m
@@ -0,0 +1,9 @@
+function x = sincos(p,m)
+
+f = 50/500;
+
+if p==0
+    x = %%TBC
+else
+    x = %%TBC
+end
